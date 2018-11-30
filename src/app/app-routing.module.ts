ea4a9d5172e0b6c820e10160c728062b1b5fc67a
TwoDCube <2dcube01@gmail.com>
2018-11-30 03:21:02 +0100

    chore: Change default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'register',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
